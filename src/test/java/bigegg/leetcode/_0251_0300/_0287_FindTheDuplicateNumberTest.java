32ae550ce2c2d84aae846fc02bf8bc2167cf53f1
BigEgg <cd_bigegg@hotmail.com>
2020-09-03 15:36:54 -0700

    Fix the error

--- src/test/java/bigegg/leetcode/_0251_0300/_0287_FindTheDuplicateNumberTest.java
+++ src/test/java/bigegg/leetcode/_0251_0300/_0287_FindTheDuplicateNumberTest.java
@@ -10,7 +10,7 @@ public class _0287_FindTheDuplicateNumberTest {
     public void testFindDuplicate()
     {
         _0287_FindTheDuplicateNumber solution = new _0287_FindTheDuplicateNumber();
-        assertEquals(2, solution.findDuplicate(new Integer[] { 1, 3, 4, 2, 2 }));
-        assertEquals(3, solution.findDuplicate(new Integer[] { 3, 1, 3, 4, 2 }));
+        assertEquals(2, solution.findDuplicate(new int[] { 1, 3, 4, 2, 2 }));
+        assertEquals(3, solution.findDuplicate(new int[] { 3, 1, 3, 4, 2 }));
     }
 }
