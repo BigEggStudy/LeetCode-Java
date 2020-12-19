d188d73a0b011a0de8ca11965bff4dfdd59d211c
BigEgg <cd_bigegg@hotmail.com>
2020-12-18 21:32:50 -0800

    Add Java solution for problem 144

---
+++ src/main/java/bigegg/leetcode/_1051_1100/_1099_TwoSumLessThanK.java
@@ -0,0 +1,29 @@
+//-----------------------------------------------------------------------------
+// Runtime: 44ms
+// Memory Usage: 
+// Link: 
+//-----------------------------------------------------------------------------
+
+package bigegg.leetcode._1051_1100;
+
+import java.util.Arrays;
+
+public class _1099_TwoSumLessThanK {
+    public int twoSumLessThanK(int[] A, int K) {
+        Arrays.sort(A);
+
+        int left = 0, right = A.length - 1;
+        int max = -1;
+        while (left < right) {
+            int sum = A[left] + A[right];
+            if (sum < K) {
+                left++;
+                max = Math.max(max, sum);
+            }
+            else
+                right--;
+        }
+
+        return max;
+    }
+}
