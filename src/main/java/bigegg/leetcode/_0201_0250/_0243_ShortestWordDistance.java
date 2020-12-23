702878af691dee516e2f262590ae9008c0d5776a
BigEgg <cd_bigegg@hotmail.com>
2020-12-23 11:31:23 -0800

    Add Java solution for problem 243

---
+++ src/main/java/bigegg/leetcode/_0201_0250/_0243_ShortestWordDistance.java
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------------
+// Runtime: 1ms
+// Memory Usage: 38.7 MB
+// Link: https://leetcode.com/submissions/detail/426506732/
+//-----------------------------------------------------------------------------
+
+package bigegg.leetcode._0201_0250;
+
+public class _0243_ShortestWordDistance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int i1 = -1, i2 = -1;
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                i1 = i;
+            } else if (words[i].equals(word2)) {
+                i2 = i;
+            }
+
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+            }
+        }
+        return minDistance;
+    }
+}
